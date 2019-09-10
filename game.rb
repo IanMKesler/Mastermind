1d048a3ae6715b562bff61e541188165715acc30
Ian Kesler <ianmkesler@gmail.com>
2019-09-09 18:17:29 -0700

    Add primitive random guessing with position memory

--- game.rb
+++ game.rb
@@ -10,15 +10,19 @@ class Game
   end
 
   def play
+    puts "Welcome to Mastermind! Input (M) to play as the code maker or (B) to play as the code breaker."
+    getRole == "M" ? @maker.player = true : @breaker.player = true
     puts "Selecting Secret Pattern"
-    generateSecret
+    @maker.player ? @maker.pattern = getPattern : generateSecret
     while @turn <= 12
+      #puts "Pattern: #{@maker.pattern}"
+      puts "Code goes here #{@maker.showPattern}"  #for testing
       @board.show
-      hint if @turn > 1
-      @maker.showPattern
+      hints = [0,0]
+      hints = hint if @turn > 1
       puts "Please input guess"
-      getGuess
-      @board.draw(@breaker.pattern, @turn)
+      @breaker.player ? @breaker.pattern = getPattern : generateGuess(hints)
+      @board.draw(@breaker.pattern.dup, @turn)
       if win?
         @board.show
         puts "Nice! The breaker got it!"
@@ -34,39 +38,65 @@ class Game
 
   private
 
+  def getRole
+    input = gets.strip.upcase
+    until input.match(/[MB]/)
+      puts "Please input either M or B and press enter."
+      input = gets.strip.upcase
+    end
+    input
+  end
+
+  def generateGuess(hints)
+    correct = hints[0]
+    color = hints[1]
+    indexes = [0, 1, 2, 3]
+    positions = indexes.sample(correct)
+    puts "Positions: #{positions}"
+
+    @breaker.pattern = [0, 0, 0, 0] if @breaker.pattern == []
+    @breaker.pattern.map!.with_index { |x, i|
+      positions.include?(i) ? x : x = rand(5)
+    }
+
+    
+    puts @breaker.showPattern
+  end
+
   def generateSecret
     4.times { @maker.pattern << rand(5) }
-    puts @maker.showPattern
+    #puts "Secret Code: #{@maker.showPattern}" #for testing
   end
 
-  def getGuess
-    guess = formatGuess
-    until guess.length == 4
+  def getPattern
+    pattern = formatPattern
+    until pattern.length == 4
       puts "Please input four numbers from 0-5. Ex: 0254"
-      guess = formatGuess
+      pattern = formatPattern
     end
-    @breaker.pattern = guess
+    pattern
   end
 
-  def formatGuess
-    guess = gets
-    output = guess.scan /[0-5]/
+  def formatPattern
+    pattern = gets
+    output = pattern.scan /[0-5]/
     output.map { |x| x.to_i }
   end
 
   def win?
     puts "Breaker: #{@breaker.pattern}"
-    puts "Maker: #{@maker.pattern}"
-    puts "Win?: #{@breaker.pattern == @maker.pattern}"
+    #puts "Maker: #{@maker.pattern}"
+    #puts "Win?: #{@breaker.pattern == @maker.pattern}"
     @breaker.pattern == @maker.pattern ? true : false
   end
 
   def hint
     output = compare([@maker.pattern, @breaker.pattern])
     puts "#{output[0]} are correct. #{output[1]} correct numbers but in the wrong position."
+    output
   end
 
-  def compare(input) 
+  def compare(input)
     code = input[0].dup #Arrays are not POD types, need to make a shallow copy to avoid changing original.
     guess = input[1].dup
     output = [0, 0]
@@ -77,7 +107,7 @@ class Game
         guess[i] = nil
       end
     }
-    puts "Maker after compare: #{@maker.pattern}"
+    #puts "Maker after compare: #{@maker.pattern}"
     code = code.select { |x| x != nil }
     guess = guess.select { |x| x != nil }
     code.each_with_index { |x, i|
--- player.rb
+++ player.rb
@@ -1,10 +1,11 @@
 class Player
-  attr_accessor :pattern
+  attr_accessor :pattern, :player
   attr_reader :role
 
   def initialize(role)
     @role = role
     @pattern = []
+    @player = false
   end
 
   def showPattern
